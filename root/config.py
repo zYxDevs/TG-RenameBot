5b0f1db87e5a14f54f14ec7bd73f82883f0532ba
Yoga Pranata <45356875+zYxDevs@users.noreply.github.com>
2021-11-21 01:33:40 +0700

    Update config.py

--- root/config.py
+++ root/config.py
@@ -13,7 +13,7 @@ class Config(object):
   TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
   AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
   DOWNLOAD_LOCATION = "./bot/DOWNLOADS"
-  WITH_FFPROBE = os.environ.get("WITH_FFPROBE", "false")
+  WITH_FFPROBE = os.environ.get("WITH_FFPROBE", "False")
   DB_URI = os.environ.get("DATABASE_URL", "")
   # owner is for log cmd only owner can use (this can be multiple users)
   OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "").split(" ")]
